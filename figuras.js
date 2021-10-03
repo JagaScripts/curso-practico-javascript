d69db064986e82a3e06a7ba76038325eafafb5fd
JoseAGonzalezA <jose.a.gonzalez.alcantara@gmail.com>
2021-10-03 06:50:42 +0200

    6:50 2/10/2021 Calculando figuras geométricas en JavaScript v7 7:10

--- figuras.js
+++ figuras.js
@@ -1,7 +1,7 @@
 //console.log("Figuras geométricas");
 
 // Códig del cuadrado
-console.group("Cuadrado");
+console.group("Cuadrados");
 const ladoCuadrado = 5;
 console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
 
@@ -13,12 +13,36 @@ console.log(`El area del cuadrado miden: ${areaCuadrado} cm^2`);
 console.groupEnd();
 
 // Códig del triángulo
-console.group("Triangulo");
+console.group("Triangulos");
 const baseTriangulo = 4;
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
+const alturaTriangulo = 5.5;
 console.log(`Los lados del triangulo miden: 
 ${baseTriangulo} cm, 
 ${ladoTriangulo1} cm, 
 ${ladoTriangulo2} cm `);
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.groupEnd();
+
+// Código del circulo
+console.group("Circulos");
+
+// Radio
+const radioCirculos = 4;
+
+//Diámetro
+const diametroCirculo = radioCirculos * 2;
+
+//Pi 
+//const PI = 3.1415;
+const PI = Math.PI;
+console.log(PI);
+// Circuferencia
+const perimetroCirculo = diametroCirculo * PI;
+
+// Área
+const areaCirculo = (radioCirculos * radioCirculos) * PI;
+
 console.groupEnd();
